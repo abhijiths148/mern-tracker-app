9b5c4e305f07b0136d9c7c76b4efb34c0b8fd31e
Abhijith S <abhijiths148@gmail.com>
2020-02-02 14:19:29 +0530

    completing simple CRUD backend

--- backend/server.js
+++ backend/server.js
@@ -10,13 +10,19 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+const uri = process.env.ATLAS_URI || "";
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB connected successfully")
 });
+
+const exerciseRouter = require('./routes/exercise');
+const userRouter = require('./routes/user');
+
+app.use('/exercises', exerciseRouter);
+app.use('/users', userRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
